32e4b172b182d8672064236e3bfeb23711e45857
LakshmeeshH <lakshmeesh.h@gmail.com>
2018-09-15 19:22:29 +0530

    Connecting MongoDB with mongoose

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  monogoURI:
+    'mongodb://lakshmeesh:lakki1@ds157522.mlab.com:57522/lakkidevconnector'
+};
--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+//DB Config
+const db = require('./config/keys').monogoURI;
+
+//Connect to Mongo DB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('Hello!'));
 
